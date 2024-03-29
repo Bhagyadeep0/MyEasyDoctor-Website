cd8dd5064995eb7a1f9240935efe0f9c15912d5b
Bhagyadeep <b.ghosh0330@gmail.com>
2024-03-29 19:12:04 +0530

    files added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// global css
+import './global.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='h1 text-blue-700 '>MyEasyDoctor!</h1>
+    </div>
+  )
+}
+
+export default App
