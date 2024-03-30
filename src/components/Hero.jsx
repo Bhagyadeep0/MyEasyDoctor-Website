d01a80bb48233b9df9764b17dd757b73696f5eef
Bhagyadeep <b.ghosh0330@gmail.com>
2024-03-30 13:53:58 +0530

    updated files

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,16 +12,18 @@ const Hero = () => {
     <div className="w-full h-full mt-[90px] xl:h-[80vh] bg-celeste relative">
       <Navigation />
       {/* hero centered */}
-      <div className="flex justify-center w-full h-full "> 
+      <div className="flex justify-center w-full h-full ">
         {/* contents */}
         <motion.div className="flex flex-col lg:flex-row justify-between items-center container m-auto">
           {/* text */}
-          <div className="flex-1 text-center lg:max-w-[50%] py-3 px-4">
+          <div className="flex-1 text-center lg:text-left lg:max-w-[50%] py-3 px-4">
+            {/* title */}
             <h1 className="h1">
               <span className="text-tiffanyBlue">MyEasyDoctor</span>
               <br />
               Where <i>Appointments</i> Meet <i>Convenience</i>.
             </h1>
+            {/* desc */}
             <p className="text-red-500 mb-4 p">
               MyEasyDoctor is like a helpful digital assistant for your health!
               It's a safe and flexible platform that makes it easy for hospitals
@@ -30,47 +32,48 @@ const Hero = () => {
               improves how doctors and patients talk to each other. It's all
               about making healthcare simpler and better for everyone!
             </p>
-              {/* image & animation on small screen */}
-          <div className=" lg:hidden flex relative pointer-events-none">
-          {/* image */}
-            <div className="imageContainer flex-1 flex justify-center rounded-full ">
-              <img src="/doctor01.png" alt="doctor.png" width={300}  />
+            {/* image & animation on small screen */}
+            <div className=" lg:hidden flex relative pointer-events-none">
+              {/* image */}
+              <div className="imageContainer flex-1 flex justify-center rounded-full ">
+                <img src="/doctor01.png" alt="doctor.png" width={300} />
+              </div>
+              {/* floating elements */}
+              <div className="text-xl">
+                <p className="flex items-center gap-2 absolute bottom-[15.5em] left-[3em] bg-white rounded-full px-2 font-semibold animate-left-right">
+                  <span>
+                    <SiSpeedtest />{" "}
+                  </span>
+                  <span>Fast & Secure</span>
+                </p>
+                <p className="flex items-center gap-2 absolute bottom-[9em] left-[1em] bg-white rounded-full px-2 font-semibold animate-left-right">
+                  <span>
+                    <FaUserDoctor />{" "}
+                  </span>
+                  <span>Talk to Doctor</span>
+                </p>
+                <p className="flex items-center gap-2 absolute top-[9em] right-[3em] bg-white rounded-full px-2 font-semibold animate-left-right">
+                  <span>
+                    <FaRegSave />{" "}
+                  </span>
+                  <span>Save Records</span>
+                </p>
+                <p className="flex items-center gap-2 absolute top-[15.5em] right-[6em] bg-white rounded-full px-2 font-semibold animate-left-right">
+                  <span>
+                    <SiSpeedtest />{" "}
+                  </span>
+                  <span>Fast & Secure</span>
+                </p>
+              </div>
             </div>
-            {/* floating elements */}
-            <div className="text-xl">
-              <p className="flex items-center gap-2 absolute bottom-[15.5em] left-[3em] bg-white rounded-full px-2 font-semibold animate-left-right">
-                <span>
-                  <SiSpeedtest />{" "}
-                </span>
-                <span>Fast & Secure</span>
-              </p>
-              <p className="flex items-center gap-2 absolute bottom-[9em] left-[1em] bg-white rounded-full px-2 font-semibold animate-left-right">
-                <span>
-                  <FaUserDoctor />{" "}
-                </span>
-                <span>Talk to Doctor</span>
-              </p>
-              <p className="flex items-center gap-2 absolute top-[9em] right-[3em] bg-white rounded-full px-2 font-semibold animate-left-right">
-                <span>
-                  <FaRegSave />{" "}
-                </span>
-                <span>Save Records</span>
-              </p>
-              <p className="flex items-center gap-2 absolute top-[15.5em] right-[6em] bg-white rounded-full px-2 font-semibold animate-left-right">
-                <span>
-                  <SiSpeedtest />{" "}
-                </span>
-                <span>Fast & Secure</span>
-              </p>
-            </div>
-          </div>
+            {/* button */}
             <button className="bg-red-400 mt-5 py-3 px-5 rounded-md text-xl font-medium hover:bg-red-600 transition-all duration-300 ease-in">
               Watch Intro
             </button>
           </div>
           {/* image & animation on large screen */}
           <div className=" hidden lg:flex relative pointer-events-none ">
-          {/* image */}
+            {/* image */}
             <div className="imageContainer">
               <img src="/doctor01.png" alt="doctor.png" />
             </div>
@@ -78,26 +81,34 @@ const Hero = () => {
             <div className="text-xl">
               <p className="flex items-center gap-2 absolute bottom-[15.5em] left-[-6em] bg-white rounded-full px-2 font-semibold animate-left-right">
                 <span>
-                  <SiSpeedtest />{" "}
+                  {/* logo */}
+                  <SiSpeedtest />
                 </span>
+                {/* text */}
                 <span>Fast & Secure</span>
               </p>
               <p className="flex items-center gap-2 absolute bottom-[5em] left-[1em] bg-white rounded-full px-2 font-semibold animate-left-right">
                 <span>
+                  {/* logo */}
                   <FaUserDoctor />{" "}
                 </span>
+                {/* text */}
                 <span>Talk to Doctor</span>
               </p>
               <p className="flex items-center gap-2 absolute top-[12em] right-[6em] bg-white rounded-full px-2 font-semibold animate-left-right">
                 <span>
+                  {/* logo */}
                   <FaRegSave />{" "}
                 </span>
+                {/* text */}
                 <span>Save Records</span>
               </p>
               <p className="flex items-center gap-2 absolute top-[17.5em] right-[2em] bg-white rounded-full px-2 font-semibold animate-left-right">
                 <span>
+                  {/* logo */}
                   <SiSpeedtest />{" "}
                 </span>
+                {/* text */}
                 <span>Fast & Secure</span>
               </p>
             </div>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,3 +1,6 @@
+//import framer motion
+import { easeInOut, motion, useCycle, AnimatePresence } from "framer-motion";
+
 const Navigation = () => {
   // navigation data
   const navData = [
@@ -7,38 +10,139 @@ const Navigation = () => {
     { name: "register", path: "/register" },
   ];
 
+  const [toggle, setToggle] = useCycle(false, true);
+
   return (
     <nav className=" w-full bg-white shadow-md fixed top-0 z-[99]">
-      <ul className=" container m-auto h-[90px] px-4 flex justify-between items-center">
-        <div className="flex cursor-pointer">
+      <ul className=" container m-auto w-full h-[90px] px-4 flex justify-between items-center">
+        {/* logo */}
+        <div className="cursor-pointer">
           <h1 className="text-2xl">Logo</h1>
         </div>
-        {/* small screen */}
-        <button>
-          Button
-         <div className="absolute bg-gunMetal ">
-         {navData.map((item, index) => (
-            <a
-              href={item.path}
-              key={index}
-              className="flex text-2xl font-medium text-bitterSweet capitalize focus:outline-none"
+        {/* links */}
+        <div>
+          {/* small screen */}
+          <div className="flex lg:hidden">
+            {/* toggle button */}
+            <motion.button
+              onClick={() => setToggle()} // change toggle value
+              animate={toggle ? "open" : "close"}
+              className="flex flex-col justify-center space-y-1 border-[6px] py-3 px-2 border-red-400 w-fit h-fit outline-none items-center z-[999]"
             >
-              {item.name}
-            </a>
-          ))}
-         </div>
-        </button>
+              <motion.span
+                variants={{
+                  close: { rotate: 0, y: 0 },
+                  open: { rotate: 50, y: "5px" },
+                }}
+                className="w-10 h-1 bg-black"
+              ></motion.span>
+              <motion.span
+                variants={{
+                  close: { rotate: 0, y: 0 },
+                  open: { rotate: -50, width: "40px" },
+                }}
+                className="w-8 h-1 bg-black"
+              ></motion.span>
+              <motion.span
+                variants={{
+                  close: { rotate: 0, opacity: 1 },
+                  open: { rotate: 0, opacity: 0 },
+                }}
+                className="w-5 h-1 bg-black"
+              ></motion.span>
+            </motion.button>
+            {/* show when toggle */}
+            <div>
+              {/* toggle menu */}
+              <AnimatePresence>
+                {toggle && (
+                  <motion.div
+                    initial="close"
+                    animate="open"
+                    exit="close"
+                    variants={{
+                      close: {
+                        x: "-100%",
+                        transition: {
+                          staggerChildren: 0.25,
+                          when: "afterChildren",
+                          duration: 0.5,
+                          ease: easeInOut,
+                        },
+                      },
+                      open: {
+                        x: "0%",
+                        transition: {
+                          staggerChildren: 0.25,
+                          when: "beforeChildren",
+                          duration: 0.35,
+                          ease: easeInOut,
+                        },
+                      },
+                    }}
+                    className="absolute flex flex-col justify-center items-center top-0 left-0 gap-y-[50px] text-3xl bg-blue-600 h-screen w-full container m-auto"
+                  >
+                    {/* logo */}
+                    <motion.div
+                      variants={{
+                        close: { y: "-100%", opacity: 0 },
+                        open: { y: "0%", opacity: 1 },
+                      }}
+                    >
+                      <h1>LOGO</h1>
+                    </motion.div>
+                    {/* links */}
+                    <motion.div
+                      variants={{
+                        close: { y: "-20%", opacity: 0 },
+                        open: { y: "0%", opacity: 1 },
+                      }}
+                      className="space-y-10"
+                    >
+                      {navData.map((item, index) => (
+                        <a
+                          href={item.path}
+                          key={index}
+                          className="flex justify-center text-3xl font-medium text-bitterSweet capitalize focus:outline-none"
+                        >
+                          {item.name}
+                        </a>
+                      ))}
+                    </motion.div>
+                    {/* socials */}
+                    <motion.div
+                      variants={{
+                        close: { y: "-20%", opacity: 0 },
+                        open: { y: "0%", opacity: 1 },
+                      }}
+                    >
+                      Social
+                    </motion.div>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </div>
+          </div>
 
-        {/* large screen  */}
-        {navData.map((item, index) => (
-          <a
-            href={item.path}
-            key={index}
-            className="hidden md:flex text-2xl font-medium text-bitterSweet capitalize focus:outline-none"
-          >
-            {item.name}
-          </a>
-        ))}
+          {/* large screen  */}
+          <div className="flex items-center justify-between gap-x-5">
+            <div className="flex gap-x-8">
+              {navData.map((item, index) => (
+                <div>
+                  <a
+                    href={item.path}
+                    key={index}
+                    className="hidden lg:flex text-2xl font-medium text-bitterSweet capitalize focus:outline-none"
+                  >
+                    {item.name}
+                  </a>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+         {/* social */}
+         <div className="hidden lg:flex text-2xl">Social</div>
       </ul>
     </nav>
   );
